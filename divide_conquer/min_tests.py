0ecfbeef51f344f34d2c4857388af38b3bb30a8e
jamesben6688 <jamesben6688@gmail.com>
2025-05-09 01:54:42 -0700

    update

--- divide_conquer/min_tests.py
+++ divide_conquer/min_tests.py
@@ -2,10 +2,75 @@
 given n tasks. 1-n. there is a pair of tasks that will result in unit test failure.
 find the pair in fastest way. (3 methods in total, need to make assumption about how to run test)
 
-平均O(nlg(N)), 最坏 O(N^2)
+平均O(nlg(N))
 """
+def find_bad_pair_O_lgN(tasks, test):
+    """
+
+        O(lgN)
+    :param tasks:
+    :param test:
+    :return:
+    """
+    left = 0
+    l = left + 1
+    r = len(tasks) - 1
+
+    while l < r:  # [)
+        mid = l + (r-l) // 2
+
+        if not test(tasks[left: mid+1]):
+            r = mid
+        else:
+            l = mid + 1
+    bad_2 = l
+
+    l = 0
+    r = bad_2
+
+    while l < r:  # (]
+        mid = l + (r-l+1) // 2
+        if not test(tasks[mid:bad_2+1]):
+            l = mid
+        else:
+            r = mid - 1
+
+    bad_1 = l
+    return [bad_1, bad_2]
+
+
+
+
+def find_bad_pair_O_N(tasks, test):
+    left = 0
+    right = len(tasks)-1
+
+    bad_1 = None
+    bad_2 = None
+
+    while left <= right:
+        if not test(tasks[left: right+1]):
+            left += 1
+        else:
+            bad_1 = left - 1
+            break
+
+    while left <= right:
+        if not test(tasks[bad_1: right+1]):
+            right -= 1
+        else:
+            bad_2 =  right+1
+            break
+
+    return [bad_1, bad_2]
 
 def find_bad_pair_fast(tasks, test):
+    """
+        O(Nlg(N))
+    :param tasks:
+    :param test:
+    :return:
+    """
     def helper(subtasks):
         if len(subtasks) <= 2:
             return tuple(subtasks)
@@ -41,11 +106,13 @@ def make_test_function(bad_pair):
     return test
 
 # Example tasks and bad pair
-tasks = list(range(1, 101))
-bad_pair = (23, 77)  # Example bad pair
+tasks = list(range(0, 101))  # index 从0开始
+bad_pair = (2, 77)  # Example bad pair
 
 test_fn = make_test_function(bad_pair)
 result = find_bad_pair_fast(tasks, test_fn)
+print(find_bad_pair_O_lgN(tasks, test_fn))
+print(find_bad_pair_O_N(tasks, test_fn))
 
 print("Bad pair found:", result)
 
